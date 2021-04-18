41f1de5f53373d4405db2887623664e44fb47385
Rithish-vt <66373962+Rithish-vt@users.noreply.github.com>
2021-04-18 20:54:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,16 +48,15 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b> @KGMoviez </b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
 
-⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-"""
+
+
+
+
